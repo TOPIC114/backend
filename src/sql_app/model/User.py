35b2d61236c80db3a94c5b410268d1a502f685f5
bloodnighttw <emails@bntw.dev>
2024-04-06 16:01:53 +0800

    fix and update model in User.py & test.py!

--- src/sql_app/model/User.py
+++ src/sql_app/model/User.py
@@ -14,6 +14,7 @@ class User(Base):
     email = Column(String(255), nullable=False, unique=True)
     password = Column(String(24), nullable=False)
     level = Column(Integer, nullable=False)  # permission level
+    sessions = relationship('Session', backref='user', passive_deletes=True)
 
 
 def get_expire_date():
@@ -23,12 +24,7 @@ def get_expire_date():
 class Session(Base):
     __tablename__ = 'session'
     id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    session = Column(String(24), nullable=False, primary_key=True)
+    session = Column(String(24), nullable=False, primary_key=True, unique=True)
     expire = Column(DateTime, nullable=True, default=get_expire_date)
 
 
-class History(Base):
-    __tablename__ = 'user_history'
-    id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    history = relationship(Recipe)
-    time = Column(DateTime, nullable=False, default=datetime.now)
