be8aadef268d8ae8e3007b4f1afd96bcaf451e75
bloodnighttw <emails@bntw.dev>
2024-04-08 22:27:12 +0800

    Add user router

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI, APIRouter
 
 from test import test_router
+from user import user_root
 
 logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
@@ -21,3 +22,4 @@ async def say_hello(name: str):
 
 app.include_router(router)
 app.include_router(test_router)
+app.include_router(user_root)
