89e924a8ae13f58ea135ca4f4ceb38a2e0d54262
bloodnighttw <emails@bntw.dev>
2024-04-08 13:31:25 +0800

    add Recipe Ingredient Many-to-Many relationship.

--- src/sql_app/model/Recipe.py
+++ src/sql_app/model/Recipe.py
@@ -1,7 +1,13 @@
 from sqlalchemy.orm import relationship
 
 from ..db import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, ForeignKey
+
+made = Table(
+    'made', Base.metadata,
+    Column('rid', Integer, ForeignKey('Recipe.id', ondelete='CASCADE')),
+    Column('iid', Integer, ForeignKey('ingredient.id', ondelete='CASCADE'))
+)
 
 
 class Recipe(Base):
@@ -13,6 +19,7 @@ class Recipe(Base):
     comments = relationship('Comment', backref='Recipe', passive_deletes=True)
     searches = relationship('User', secondary='search', backref='Recipe', passive_deletes=True, lazy='dynamic')
     authors = relationship('User', secondary='author', backref='Recipe', passive_deletes=True, lazy='dynamic')
+    made = relationship('Ingredient', secondary=made, backref='Recipe', passive_deletes=True, lazy='dynamic')
 
 
 class Recipe_Type(Base):
@@ -22,9 +29,9 @@ class Recipe_Type(Base):
 
 
 class Ingredient(Base):
-    __tablename__ = 'Ingredient'
+    __tablename__ = 'ingredient'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(32), nullable=False)
     weight = Column(Integer, nullable=False)
     mandarin = Column(String(16), nullable=False)
-
+    made = relationship('Recipe', secondary=made, backref='Ingredient', passive_deletes=True, lazy='dynamic')
