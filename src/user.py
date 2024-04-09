424f2d310bb6831eef82663f433d422ecbb13c3f
bloodnighttw <emails@bntw.dev>
2024-04-09 15:59:04 +0800

    add search history and logout endpoint.

--- src/user.py
+++ src/user.py
@@ -2,14 +2,15 @@ import uuid
 
 from fastapi import APIRouter, HTTPException, Security, Depends
 from fastapi.security import APIKeyHeader
-from sqlalchemy import select, or_
+from sqlalchemy import select, or_, delete
 from sqlalchemy.exc import IntegrityError
 
 from request.user import RegisterRequest, LoginRequest
 from response.user import UserInfoResponse
 from response.utils import SuccessResponse
 from sql_app.db import AsyncDBSession
-from sql_app.model.User import User, Session
+from sql_app.model.Recipe import Recipe
+from sql_app.model.User import User, Session, search
 
 user_root = APIRouter(
     prefix='/user',
@@ -75,3 +76,28 @@ async def token_verify(db: AsyncDBSession, token: str = Security(api_key_header)
 })
 async def get_user_info(user: User = Depends(token_verify)) -> UserInfoResponse:
     return {'id': user.id, 'username': user.username, 'email': user.email, 'level': user.level}
+
+
+@user_root.get('/searches', status_code=200, responses={
+    401: {"description": "Unauthorized - Invalid token"}
+})
+async def get_user_search_history(db: AsyncDBSession, user: User = Depends(token_verify)):
+    stmt = select(Recipe).join_from(search, Recipe).where(search.c.uid == user.id)
+    result = await db.execute(stmt)
+    recipe_list = result.scalars().all()
+    return recipe_list
+
+
+@user_root.get('/logout', status_code=200, responses={
+    401: {"description": "Unauthorized - Invalid token"}
+})
+async def token_verify(db: AsyncDBSession, token: str = Security(api_key_header)) -> SuccessResponse:
+    stmt = select(Session).join_from(User, Session).where(Session.session == token).limit(1)
+    result = await db.execute(stmt)
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(status_code=401, detail='Invalid token')
+
+    stmt = delete(Session).where(Session.session == token)
+    await db.execute(stmt)
+    return {'message': 'Logout success'}
