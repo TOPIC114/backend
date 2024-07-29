05a4c6747b59d885ac349e80cc572332181e2cc4
bloodnighttw <emails@bntw.dev>
2024-07-29 20:40:50 +0800

    add comment.

--- src/sql_app/model/Video.py
+++ src/sql_app/model/Video.py
@@ -11,7 +11,7 @@ class Video(Base):
     yt_link = Column(String(128), unique=True)
     thumbnail_url = Column(String(128))
     author_id = Column(String(16))
-    is_reviewed = Column(Boolean, default=None, nullable=True)
+    is_reviewed = Column(Boolean, default=None, nullable=True)  # null mean not ready to review or detected.
     is_complete = Column(Boolean, default=False)
 
 
