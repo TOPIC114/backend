018300b03f4998c871e0c393942d55d7416a7577
bloodnighttw <emails@bntw.dev>
2024-11-11 01:27:28 +0800

    main description.

--- src/main.py
+++ src/main.py
@@ -10,8 +10,42 @@ from user import user_root
 from recipe import recipe_root
 from video import video_root
 
+description = """
+# BACKEND
 
-app = FastAPI()
+### This is the backend of the project. It provides the API for the frontend to interact with the database and the AI model.
+
+## Implemented Guidelines
+1. If the endpoint has a detailed description, you can implement the endpoint directly. Because this type of endpoint
+is considered as stable.
+
+2. If the endpoint has no detailed description, you are still able to implement the endpoint, but you should be aware
+that the endpoint may change in the future.
+
+3. If the endpoint description said that it's not implemented, you should not implement the endpoint, 
+because the endpoint is not implemented in the backend yet.
+4. The endpoint that is described as "Token Required" means that the endpoint requires a token to access, you should put 
+the token in the header with the key name `X-API-Key`.
+
+5. The endpoint that is described as "Admin Only" means that the endpoint requires the user to have the admin permission 
+level to access. The admin permission level is 128.
+
+6. The endpoint that is described as "Stuffs" means that the endpoint is not implemented yet or only can be accessed by 
+admin, but it will be implemented in the future.
+
+## Note
+FastAPI is using the OpenAPI standard, so you can use this Swagger UI to interact with the API.
+
+You can also generate the client code from the OpenAPI standard, so you can use the client code to interact with the API.
+
+### That's all the guidelines for now. If you have any questions, please ask in line/discord.
+
+"""
+
+
+app = FastAPI(
+    description=description,
+)
 
 app.include_router(user_root)
 app.include_router(recipe_root)
