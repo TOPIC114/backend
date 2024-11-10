e2c19022d1c22ee52fd060b27428815eab10bfd4
bloodnighttw <emails@bntw.dev>
2024-11-10 18:55:13 +0800

    Update non-async code to async

--- src/detect.py
+++ src/detect.py
@@ -1,3 +1,5 @@
+import asyncio
+import concurrent.futures
 import copy
 import io
 import json
@@ -9,29 +11,25 @@ from typing import List
 
 import PIL
 import aiofiles
+import cv2
+import ffmpeg
+import google.generativeai as genai
 import numpy as np
-import requests
+import aiohttp
 from PIL import Image, ImageDraw
-from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
+from fastapi import APIRouter, UploadFile, File, Depends, HTTPException, BackgroundTasks
+from google.ai.generativelanguage_v1beta.types import content
+from sahi import AutoDetectionModel
+from sahi.predict import get_sliced_prediction
 from sqlalchemy import select, update, delete
 from starlette.concurrency import run_in_threadpool
 from ultralytics import YOLO
-from sahi import AutoDetectionModel
-from sahi.predict import get_sliced_prediction
-import asyncio
-import concurrent.futures
 
 from sql_app.db import AsyncDBSession
 from sql_app.model.Model import Model
 from sql_app.model.Recipe import Ingredient
 from sql_app.model.User import User
 from user import token_verify
-
-import cv2
-import ffmpeg
-
-import google.generativeai as genai
-from google.ai.generativelanguage_v1beta.types import content
 
 detection_router = APIRouter(prefix="/detect", tags=['detect'])
 limit = 0.2  # seconds
@@ -41,17 +39,9 @@ logger = logging.getLogger(__name__)
 detect_process_pool = concurrent.futures.ProcessPoolExecutor(max_workers=2)
 
 # each ingredient's confidence threshold
-confidence_filter = {
-    "mushroom": 0.85,
-    "okra": 0.75,
-    "heim": 0.85,
-    "beef": 0.4,
-    "chicken": 0.4,
-    "pork": 0.4,
-    "noodle": 0.85,
-    "carrot": 0.5,
-    "common": 0.65  # the ingridient which is not in the filter
-}
+confidence_filter = {"mushroom": 0.85, "okra": 0.75, "heim": 0.85, "beef": 0.4, "chicken": 0.4, "pork": 0.4,
+                     "noodle": 0.85, "carrot": 0.5, "common": 0.65  # the ingridient which is not in the filter
+                     }
 
 
 def result_processing(img, results):  # results:[{"key":[(x1,x2,y1,y2)]}]
@@ -69,11 +59,7 @@ def result_processing(img, results):  # results:[{"key":[(x1,x2,y1,y2)]}]
 
 
 def image_processing(image, model_path):
-    model = AutoDetectionModel.from_pretrained(
-        model_type="yolov8",
-        device="cuda:0",
-        model_path=model_path,
-    )
+    model = AutoDetectionModel.from_pretrained(model_type="yolov8", device="cuda:0", model_path=model_path, )
     results = get_sliced_prediction(image, model, slice_height=500, slice_width=500, overlap_height_ratio=0.2,
                                     overlap_width_ratio=0.2)
     result = {}
@@ -165,8 +151,7 @@ async def detect_image(version: str, db: AsyncDBSession, image: UploadFile = Fil
 
 
 @detection_router.post("/upload/pt")
-async def upload_pt(db: AsyncDBSession, description: str, version: str,
-                    user: User = Depends(token_verify),
+async def upload_pt(db: AsyncDBSession, description: str, version: str, user: User = Depends(token_verify),
                     pt: UploadFile = File(...)):
     if user.level <= 127:
         raise HTTPException(status_code=401, detail='You are not administrator')
@@ -197,12 +182,9 @@ async def upload_pt(db: AsyncDBSession, description: str, version: str,
     if result:
         path: str = result.file_path
         os.remove(path)
-        stmt = (update(Model).where(Model.version == version).values(
-            file_path=filename,
-            description=description,
-            size=size,
-            update_date=datetime.now()
-        ))
+        stmt = (
+            update(Model).where(Model.version == version).values(file_path=filename, description=description, size=size,
+                                                                 update_date=datetime.now()))
         try:
             await db.execute(stmt)
             await db.commit()
@@ -211,13 +193,8 @@ async def upload_pt(db: AsyncDBSession, description: str, version: str,
             raise e
 
     else:
-        model_information = Model(
-            file_path=filename,
-            description=description,
-            size=size,
-            version=version,
-            update_date=datetime.now()
-        )
+        model_information = Model(file_path=filename, description=description, size=size, version=version,
+                                  update_date=datetime.now())
 
         try:
             db.add(model_information)
@@ -399,16 +376,23 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 chat_session = None
 list_of_i = []
+ingredients_set = set()
 lock = asyncio.Lock()
 
 
 # If ingredients change, server will need to restart to update the chat session
-async def get_chat_session(ingredients):
+async def get_chat_session(db:AsyncDBSession):
+
     async with lock:
-        global chat_session,list_of_i
+        global chat_session, list_of_i, ingredients_set
+
+        result = await db.execute(select(Ingredient.name))
+        ingredients = [i.name for i in result]
+
         if list_of_i != ingredients:
-            chat_session = None # reset chat session
+            chat_session = None  # reset chat session
             list_of_i = ingredients
+            ingredients_set = set(ingredients)
         if chat_session is None:
             await run_in_threadpool(init_session, ingredients)
         return copy.deepcopy(chat_session)
@@ -422,53 +406,37 @@ def init_session(ingredients):
         "top_p": 0.95,
         "top_k": 40,
         "max_output_tokens": 8192,
-        "response_schema": content.Schema(
-            type=content.Type.OBJECT,
-            properties={
-                "result": content.Schema(
-                    type=content.Type.ARRAY,
-                    items=content.Schema(
-                        type=content.Type.STRING,
-                    ),
-                ),
-            },
-        ),
+        "response_schema": content.Schema(type=content.Type.OBJECT, properties={
+            "result": content.Schema(type=content.Type.ARRAY,items=content.Schema(type=content.Type.STRING)),
+        }),
         "response_mime_type": "application/json",
     }
 
-    model = genai.GenerativeModel(
-        model_name="gemini-1.5-pro",
-        generation_config=generation_config,
-        system_instruction="Detect food in image (but not text) and map to the name I provided.",
-    )
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro", generation_config=generation_config,
+                                  system_instruction="Detect food in image (but not text) and map to the name I provided.", )
 
-    history = [
-        {
-            "role": "user",
-            "parts": [
-                "Detect these shape of food which is on the list " + str(
-                    ingredients) + " on these images down below, ,than map the detected ingredients to the name i provided as an array with field name \"result\". "
-            ],
-        },
-    ]
+    history = [{
+        "role": "user",
+        "parts": [
+            "Detect these shape of food which is on the list " +
+            str(ingredients) + " on these images down below," +
+            "than map the detected ingredients to the name i provided as an array with field name \"result\". "
+        ],
+    }]
 
-    chat_session = model.start_chat(
-        history=history
-    )
+    chat_session = model.start_chat(history=history)
 
 
 # This function only supports .jpg or .png files,
 # and if the file is not a .jpg or .png file, it will
-# Why rein
-# TODO: make it async call
-def upload2gemini(path):
+async def upload2gemini(path):
     # Using Pillow to check if the file is a .jpg or .png file
     try:
         with PIL.Image.open(path) as img:
             if img.format not in ['JPEG', 'PNG']:
                 raise HTTPException(status_code=415, detail='This endpoint only support jpg or png file')
     except PIL.UnidentifiedImageError:
-        raise HTTPException(status_code=415, detail='This endpoint is not the type that can be identified by Pillow')
+        raise HTTPException(status_code=415, detail='We can\'t recognize the file type')
 
     size_of_file = os.path.getsize(path)
 
@@ -476,102 +444,80 @@ def upload2gemini(path):
         'X-Goog-Upload-Protocol': 'resumable',
         'X-Goog-Upload-Command': 'start',
         'Content-Type': 'application/json',
-        'X-Goog-Upload-Header-Content-Length': str(size_of_file),  # header only support str, not int
+        'X-Goog-Upload-Header-Content-Length': str(size_of_file) # header only support str, not int
     }
 
-    params = {
-        'key': GOOGLE_API_KEY,
-    }
+    params = {'key': GOOGLE_API_KEY}
 
     filename = os.path.basename(path)
 
     data = "{'file': {'display_name': '" + filename + "'}}"
 
-    response = requests.post(
-        f'{BASE_URL}/upload/v1beta/files',
-        params=params,
-        headers=headers,
-        data=data
-    )
+    # response = requests.post(f'{BASE_URL}/upload/v1beta/files', params=params, headers=headers, data=data)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'{BASE_URL}/upload/v1beta/files', params=params, headers=headers, data=data) as response:
+            if response.status != 200:
+                raise HTTPException(status_code=response.status_code,
+                                    detail="Error when upload image to gemini server. details: " + str(await response.json()))
 
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Error when upload image to gemini server. details: " + str(response.json()))
+        upload_url = response.headers["x-goog-upload-url"]
 
-    upload_url = response.headers["x-goog-upload-url"]
+        headers = {
+            'X-Goog-Upload-Offset': '0',
+            'X-Goog-Upload-Command': 'upload, finalize',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-    headers = {
-        'X-Goog-Upload-Offset': '0',
-        'X-Goog-Upload-Command': 'upload, finalize',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
+        async with aiofiles.open(path, 'rb') as file:
+            data = await file.read()
 
-    with open(path, 'rb') as f:
-        data = f.read()
+        # res = requests.post(f'{upload_url}', headers=headers, data=data).json()['file']
+        async with session.post(f'{upload_url}', headers=headers, data=data) as response:
+            if response.status != 200:
+                raise HTTPException(status_code=response.status_code,
+                                    detail="Error when upload image to gemini server. details: " + str(await response.json()))
+            res = (await response.json())['file']
 
-    res = requests.post(f'{upload_url}', headers=headers, data=data).json()['file']
-    args = {
-        'name': res['name'],
-        'display_name': res['displayName'],
-        'mime_type': res['mimeType'],
-        'sha256_hash': res['sha256Hash'],
-        'size_bytes': res['sizeBytes'],
-        'state': res['state'],
-        'uri': res['uri'],
-        'create_time': res['createTime'],
-        'expiration_time': res['expirationTime'],
-        'update_time': res['updateTime']
-    }
-    return genai.types.File(args)
+        args = {
+            'name': res['name'],
+            'display_name': res['displayName'],
+            'mime_type': res['mimeType'],
+            'sha256_hash': res['sha256Hash'],
+            'size_bytes': res['sizeBytes'],
+            'state': res['state'],
+            'uri': res['uri'],
+            'create_time': res['createTime'],
+            'expiration_time': res['expirationTime'],
+            'update_time': res['updateTime']
+        }
+    
+        return genai.types.File(args)
 
-def detect_files(session,file,ingredients_sets):
+
+def detect_files(session, file):
     response = session.send_message(file)
     output = json.loads(response.text)
     result = []
 
     # ensure all the ingredients in output are on the set of ingredients
     for i in output['result']:
-        if i in ingredients_sets:
+        if i in ingredients_set:
             result.append(i)
         else:
             print(f"Warning: {i} is not in the list of ingredients.")
 
     return result
 
-# TODO: depends on get_chat_session with auto fetching ingredients list
+
+
+
 @detection_router.post("/gemini")
-async def detect_by_gemini(files:List[UploadFile] = File(...)) -> List[str]:
+async def detect_by_gemini(background:BackgroundTasks,session = Depends(get_chat_session),files: List[UploadFile] = File(...)) -> List[str]:
     """
     Detect the ingredients in the image by using the gemini API
 
     :param files: List of images
     """
-    ingredients = ['asparagus', 'avocado', 'bamboo_shoots', 'beans_green', 'beetroot', 'cassava', 'chayote', 'cinnamon',
-                   'coriander', 'corn', 'egg', 'bean_mung', 'cabbage_napa', 'carrot', 'chicken', 'crab', 'garlic',
-                   'mint', 'pepper_bell', 'potato', 'chili', 'eggplant', 'gourd_bitter', 'gourd_bottle',
-                   'gourd_pointed', 'ham', 'jackfruit', 'lemon', 'mushroom_enoki', 'onion', 'pork', 'potato_sweet',
-                   'rice', 'almond', 'apple', 'artichoke', 'banana', 'blueberry', 'broccoli', 'broccoli_white',
-                   'mustard_greens', 'spinach', 'turnip', 'butter', 'cheese', 'milk', 'pasta', 'strawberry',
-                   'ash_gourd', 'beans_red', 'bokchoy', 'bread', 'brocolli_chinese', 'cabbage', 'cucumber', 'edamame',
-                   'fish', 'mushroom', 'noodle', 'okra', 'oyster', 'pumpkin', 'radish', 'seaweed', 'taro', 'tomato',
-                   'tomato_cherry', 'clam', 'burdock', 'peanut', 'spinach_water', 'leek', 'gourd_sponge', 'salmon',
-                   'apple_wax', 'chives', 'coconut', 'dragon_fruit', 'duck', 'durian', 'frog', 'ginger', 'grape',
-                   'guava', 'heim', 'kiwi', 'lettuce', 'mango', 'melon_water', 'orange', 'papaya', 'passion_fruit',
-                   'pineapple', 'potato_leaves', 'prawn', 'spinach_chinese', 'squid', 'tofu', 'zuccini', 'bean_green',
-                   'beef', 'melon_winter', 'lamb', 'lime', 'bean_sprout', 'tofu_dried', 'tofu_skin', 'ketchup',
-                   'truffle_sauce', 'miso', 'mayonnaise', 'scallop', 'oats', 'lotus_seed', 'goji', 'jujube', 'quinoa',
-                   'tomato_paste', 'tomato_can', 'sesame_sauce', 'century_egg', 'baby_corn', 'chili_bean_sauce',
-                   'basil', 'thyme', 'stokvis', 'sweet_bean_sauce', 'shallot', 'curry', 'yogurt', 'celery', 'stock',
-                   'sesame', 'soy_sauce', 'lobster', 'crabstick', 'tofu_puff', 'honey', 'yam', 'matcha', 'bean_soy',
-                   'kimchi', 'sugar_brown', 'egg_salted', 'bacon', 'cream_whip', 'tuna_can', 'paprika',
-                   'worcestershire_sauce', 'star_anise', 'tsaoko', 'clove', 'sichuan_pepper', 'lotus_root',
-                   'dried_shrimp', 'sesame_oil', 'mirin', 'sake', 'oyster_sauce', 'chinese_sauerkraut', 'chestnut',
-                   'shaoxing_wine', 'Chinese_spirits', 'bay_leaf', 'red_wine', 'konjac', 'fish_sauce', 'ginseng',
-                   'dried_clove_fish', 'bottle_gourd', 'dried_orange_peel', 'dry_beancurd_shreds', 'shacha_sauce',
-                   'pasta_sauce', 'rice_cake', 'flour', 'gochujang_sause', 'rice-wine', 'rosemary', 'bockwurst',
-                   'indian_buead', 'euryale_seed', 'coix_seed', 'chinese_angelica', 'longan', 'whisky', 'yeast',
-                   'sichuan_lovage_rhizome', 'radix_astragali', 'cmnamomi_mmulus', 'blood', 'nutmeg', 'dumpling_skin',
-                   'black_garlic', 'drinking_yogurt']
-    session = await get_chat_session(ingredients)
 
     upload_coroutine = []
 
@@ -579,9 +525,12 @@ async def detect_by_gemini(files:List[UploadFile] = File(...)) -> List[str]:
         random_name = uuid.uuid4().hex
         async with aiofiles.open(f"./img/{random_name}", 'wb') as output_file:
             await output_file.write(await file.read())
-        upload_coroutine.append(run_in_threadpool(upload2gemini, f"./img/{random_name}"))
+        upload_coroutine.append(upload2gemini(f"./img/{random_name}"))
 
     upload_files = await asyncio.gather(*upload_coroutine)
 
-    response = await run_in_threadpool(detect_files,session,upload_files,set(ingredients))
+    response = await run_in_threadpool(detect_files, session, upload_files)
+    for i in upload_files:
+        background.add_task(run_in_threadpool,i.delete)
+
     return response
