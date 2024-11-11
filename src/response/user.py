c0c9edbd64f0be37f6ca197c5862d5b35c098884
bloodnighttw <emails@bntw.dev>
2024-11-11 09:32:49 +0800

    Add example value.

--- src/response/user.py
+++ src/response/user.py
@@ -2,7 +2,16 @@ from pydantic import BaseModel
 
 
 class UserInfoResponse(BaseModel):
-    id: int
     username: str
     email: str
     level: int
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "username": "bloodnighttw",
+                "email": "bbeenn1227@gmail.com",
+                "level": 1
+            }
+        }
+    }
