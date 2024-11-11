ad2f09d02b9fd800309fddadbafe40fef3bdf4b4
bloodnighttw <emails@bntw.dev>
2024-11-11 09:33:54 +0800

    Add example value.

--- src/response/utils.py
+++ src/response/utils.py
@@ -3,3 +3,12 @@ from pydantic import BaseModel
 
 class SuccessResponse(BaseModel):
     message: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "message": "Success!"
+            }
+        }
+    }
+
