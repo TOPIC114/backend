dc84048cd25b26f0a75eb55e1b67a15780c919e4
bloodnighttw <emails@bntw.dev>
2024-11-24 01:24:58 +0800

    add fetch sub endpoint.

--- src/made.py
+++ src/made.py
@@ -122,3 +122,36 @@ async def get_main_ingredient(rid:int,db:AsyncDBSession,user=Depends(token_verif
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=400)
+
+@made_root.get('/sub/{rid}')
+async def get_sub_ingredient(rid:int,db:AsyncDBSession,user=Depends(token_verify)) -> list:
+    """
+    # Get sub ingredient of the recipe
+
+    ## Request Body
+    - rid: recipe id
+
+    ## Response code
+    - 200: success
+    - 400: failed
+
+    ## Response Body
+    - list of sub ingredient
+    """
+
+    stmt = text(
+        """
+        SELECT iid
+        FROM made m
+        WHERE m.rid = :rid AND m.main = 0
+        """
+    )
+    try:
+        result = await db.execute(stmt,{'rid':rid})
+        response = [
+            row[0] for row in result
+        ]
+        return response
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=400)
